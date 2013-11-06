8d511097022f6b0038e9578cea8c22b01e1e4f43
Harold Poskanzer <hmp@yahoo-inc.com>
2013-11-06 11:45:07 -0800

    Add brute-force Voronoi

--- xforms/mono_voronoi.py
+++ xforms/mono_voronoi.py
@@ -6,6 +6,7 @@ Created on Aug 5, 2010
 import string
 import _xformer
 import random
+import math
 
 import ImageDraw
 import _voronoi
@@ -22,6 +23,37 @@ class Voronoi(_xformer._MonoTransformer):
         return "(%d)" % (self.args["points"])
 
     def transformImage(self, img):
+        return self.transformViaTriangulation(img)
+        #return self.transformViaBruteForce(img)
+    
+    
+    def transformViaBruteForce(self, img):
+        ret = img.copy()
+        imgx, imgy = img.size
+        nx = []
+        ny = []
+        nc = []
+        for i in range(self.args["points"]):
+            nx.append(random.randrange(imgx))
+            ny.append(random.randrange(imgy))
+            nc.append(img.getpixel((ny[-1], ny[-1])))
+        for y in range(imgy):
+            for x in range(imgx):
+                dmin = math.hypot(imgx-1, imgy-1)
+                j = -1
+                for i in range(self.args["points"]):
+                    d = math.hypot(nx[i]-x, ny[i]-y)
+                    if d < dmin:
+                        dmin = d
+                        j = i
+                ret.putpixel((x, y), nc[j])
+        #draw = ImageDraw.Draw(ret)
+        #for i in range(len(nx)):
+        #    draw.ellipse((nx[i]-2, ny[i]-2, nx[i]+2, ny[i]+2), fill=(255, 0, 0))
+        return ret
+    
+    
+    def transformViaTriangulation(self, img):
         ret = img.copy()
         pts = []
         for i in range(self.args["points"]):
@@ -30,27 +62,7 @@ class Voronoi(_xformer._MonoTransformer):
         context = _voronoi.computeVoronoiDiagram(pts)
         draw = ImageDraw.Draw(ret)
         for (pti, edges) in context.polygons.items():
-            vchain = []
-            i = 0
-            while (len(edges) > 0 and i < 1000):
-                (l, v1, v2) = edges.pop(0)
-                if (v1 < 0 or v2 < 0):
-                    continue
-                i = i + 1
-                if (len(vchain) == 0):
-                    vchain.append(v1)
-                    vchain.append(v2)
-                else:
-                    if (vchain[0] == v1):
-                        vchain.insert(0, v2)
-                    elif (vchain[0] == v2):
-                        vchain.insert(0, v1)
-                    elif (vchain[-1] == v1):
-                        vchain.append(v2)
-                    elif (vchain[-1] == v2):
-                        vchain.append(v1)
-                    else:
-                        edges.append((l, v1, v2))
+            vchain = self.getVertexChain(edges)
             vv = []
             for v in vchain:
                 vv.append(context.vertices[v])
@@ -60,6 +72,31 @@ class Voronoi(_xformer._MonoTransformer):
             #draw.ellipse((pt.x-2, pt.y-2, pt.x+2, pt.y+2), fill=(255, 0, 0))
         return ret
     
+    def getVertexChain(self, edges):
+        vchain = []
+        i = 0
+        while (len(edges) > 0 and i < 1000):
+            (l, v1, v2) = edges.pop(0)
+            if (v1 < 0 or v2 < 0):
+                continue
+            i = i + 1
+            if (len(vchain) == 0):
+                vchain.append(v1)
+                vchain.append(v2)
+            else:
+                if (vchain[0] == v1):
+                    vchain.insert(0, v2)
+                elif (vchain[0] == v2):
+                    vchain.insert(0, v1)
+                elif (vchain[-1] == v1):
+                    vchain.append(v2)
+                elif (vchain[-1] == v2):
+                    vchain.append(v1)
+                else:
+                    edges.append((l, v1, v2))
+        return vchain
+
+
     def tweakInner(self):
         self.args["points"] = random.randint(100, 2000)
         
