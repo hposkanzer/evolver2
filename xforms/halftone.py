daebc77a7813caf32ada54be8a34c662c7196956
Harold Poskanzer <hmp@yahoo-inc.com>
2013-11-11 10:00:13 -0800

    Add Halftone transformer

---
+++ xforms/halftone.py
@@ -0,0 +1,23 @@
+'''
+Created on Aug 5, 2010
+
+@author: hmp@drzeus.best.vwh.net
+'''
+import Image
+
+import mono_pil
+import _halftone
+
+
+#############################################################################
+class Halftone(mono_pil._OnOffTransformer):
+    
+    def doTransform(self, img):
+        h = _halftone.Halftone(None)
+        cmyk = h.gcr(img, 0.0)
+        dots = h.halftone(img, 0.0, cmyk, sample=5, scale=2, angles=h.default_angles)
+        ret = Image.merge('CMYK', dots)
+        ret = ret.resize(img.size, Image.ANTIALIAS)
+        return ret.convert("RGB")
+
+
