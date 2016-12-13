3e4fea64c5495a1981b5486493694aace15db887
Harold Poskanzer <harold.poskanzer@gmail.com>
2016-12-12 20:50:24 -0800

    Evolve frames by updating srcimgs symlink

--- evolve_frames.py
+++ evolve_frames.py
@@ -3,29 +3,19 @@
 import os
 import sys
 import getopt
+import shutil
 
 import Experiment
 import Creature
 import change_sources
 
-sys.path.append("/Users/hmp/workspace/evolver2")
-sys.path.append("/Users/hmp/workspace/evolver2/xforms")
-source_map_formats = {
-              "IMG_1702_\d\d\d\d.jpg": "IMG_1702_%04d.jpg",
-              "IMG_1703_\d\d\d\d.jpg": "IMG_1703_%04d.jpg",
-              "IMG_1704_\d\d\d\d.jpg": "IMG_1704_%04d.jpg",
-              "IMG_1705_\d\d\d\d.jpg": "IMG_1705_%04d.jpg",
-              "IMG_1706_\d\d\d\d.jpg": "IMG_1706_%04d.jpg",
-              "IMG_1707_\d\d\d\d.jpg": "IMG_1707_%04d.jpg"
-              }
-
 def usage( msg=None ):
     if msg:
         sys.stderr.write( "ERROR:  %s\n" % (msg) )
-    sys.stderr.write( "Usage:  %s [-m freq] -e exp -s source\n" % (os.path.basename(sys.argv[0])) )
+    sys.stderr.write( "Usage:  %s [-m freq] -e exp -c creature\n" % (os.path.basename(sys.argv[0])) )
     sys.stderr.write( "  freq:  Mutate every freq frames. Defaults to 0 (never).\n")
     sys.stderr.write( "  exp:  The name of the experiment in which to create the creature.\n")
-    sys.stderr.write( "  source:  The name of the source creature.\n")
+    sys.stderr.write( "  creature:  The name of the source creature.\n")
     sys.exit(-1)
     
     
@@ -34,36 +24,44 @@ def main():
     (odict, args) = getOptions()
     (odict, args) = processOptions(odict, args)
         
-    evolveFrames(odict, odict["e"], odict["s"], odict["m"])
+    evolveFrames(odict, odict["e"], odict["c"], odict["m"])
 
 
-def evolveFrames(odict, exp, source, mutationFreq = 0):
-    exp = Experiment.Experiment(exp)
+def evolveFrames(odict, expName, sourceName, mutationFreq = 0):
+    exp = Experiment.Experiment(expName)
     exp.loadConfig()
-    exp.loadImages()
     exp.loadTransforms()
-    source = Creature.Creature(exp, source)
+    outDir = os.path.join(exp.getCreaturesDir(), sourceName)
+    if not os.path.exists(outDir):
+        os.mkdir(outDir)
+    source = Creature.Creature(exp, sourceName)
     source.loadConfig()
     source_id = source.id
-    for frame in range(1, 101):
+    for frame in range(1, 601):
         if (mutationFreq > 0 and frame % mutationFreq == 0):
             print "Evolving..."
             source.evolve()
-        evolveFrame(odict, exp, source, source_id, frame)
+        source.id = source_id + ".%04d" % (frame)
+        print "Generating %s..." % (source.id)
+        source.saveConfig()
+        # Update the srcimgs symlink to the correct frame.
+        os.remove(exp.getSrcImageDir())
+        os.symlink(os.path.join(exp.loc.base_dir, "frames", "%04d" % (frame)), exp.getSrcImageDir())
+        # Generate it.
+        source.run()
+        # Move it into the final location.
+        src = os.path.join(exp.getCreaturesDir(), source.getImageName())
+        dst = os.path.join(outDir, "%04d.jpg" % (frame))
+        shutil.move(src, dst)
+        # Delete the extraneous files.
+        for fname in [source.getPageName(), source.getThumbName(), source.getPickleName()]:
+            os.remove(os.path.join(exp.getCreaturesDir(), fname))
         
         
-def evolveFrame(odict, exp, source, source_id, frame):
-    destination = source_id + "_%04d_3" % (frame)
-    source_map = {}
-    for (k, v) in source_map_formats.items():
-        source_map[k] = v % (frame)
-    change_sources.newCreatureInner(odict, exp, source, destination, source_map)
-    
-    
 def getOptions():
     
     try:
-        (tt, args) = getopt.getopt( sys.argv[1:], "hm:e:s:", ["help", "debug"] )
+        (tt, args) = getopt.getopt( sys.argv[1:], "hm:e:c:", ["help", "debug"] )
     except getopt.error:
         usage( str(sys.exc_info()[1]) )
 
@@ -85,7 +83,7 @@ def getOptions():
     
     if not odict.has_key("e"):
         usage()
-    if not odict.has_key("s"):
+    if not odict.has_key("c"):
         usage()
         
     return odict, args
